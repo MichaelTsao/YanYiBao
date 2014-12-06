6bcbf8b244167cb6e097e2fbe96270a5d596fe68
Michael Tsao <caoxiang@yeah.net>
2014-12-07 02:30:34 +0800

    devide config file

---
+++ protected/config/dev.php
@@ -0,0 +1,17 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+$conf = array(
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
+    'params'=>array(
+        // this is used in contact page
+        'host' => 'yyb.cx',
+    ),
+);
+$common = include('common.php');
+return array_merge_recursive($common, $conf);
